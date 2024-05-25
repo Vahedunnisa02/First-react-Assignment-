ec19c712c82fe883c2fb6ae0aa1e12f1108b2f03
vahedunnisa02 <vahedunnisavaheda@gmail.com>
2024-05-25 18:25:43 +0530

    file has been created

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='wrapper'>
+     <h1>Learn web development</h1>
+     <div>
+      <p>Welcome to the MDN learning area. THis set of articles aims to guide complete beginners to web development with all they need to start coding website.</p>
+     </div>
+
+     <div>
+      <p>The aim of this area of MDN is not to rak yor form "beginner" to "expert" but to take you from "beginner" to "comfortable". Form there , you should be able to start making your way ,learning form <a href="">the rest of MDN </a>, and other intermediate to advanced resources thar assume a lot of previous knowledge.</p>
+     </div>
+     <div>
+      <p>If you are a complete beginner,web development can ve challenging- we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home wheter you are a student learning web development (on your own or part of a class), a teaher looking for class materials , a hobbyist , or someone who just wants to understand more about how web technoligies work.</p>
+     </div>
+     </div>
+    </>
+  )
+}
+
+export default App
